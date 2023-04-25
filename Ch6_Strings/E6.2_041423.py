290eeebc611dbd1d63ec8cf6c0f0d7cf93a0f27b
Hiraeth <127773881+DaroxTurtle@users.noreply.github.com>
2023-04-24 20:46:15 -0700

    Add files via upload

---
+++ Ch6_Strings/E6.2_041423.py
@@ -0,0 +1,10 @@
+print('E6.2_041423.py')
+
+# Exercise 2: Given that fruit is a string, what does fruit[:] mean?
+
+fruit = 'banana'
+print(type(fruit))
+
+# The answer would be the whole string in the whole variable loop.
+a = fruit[:]
+print(a)
