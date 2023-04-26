6027de00d1f20731b842fb74b064aa5670ea4d24
Hiraeth <127773881+DaroxTurtle@users.noreply.github.com>
2023-04-25 17:39:41 -0700

    Create Q6.0_042523.py

---
+++ Ch6_Strings/Q6.0_042523.py
@@ -0,0 +1,11 @@
+print('Q6.0_042523.py')
+
+x = 'From marquard@uct.ac.za'
+print(len(x))
+print(x[14:17])
+
+print(len('banana')*7)
+
+data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+pos = data.find('.')
+print(data[pos:pos+3])
