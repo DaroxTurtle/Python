d7f916ab257b64f8349048ac32e00ee533445d74
Hiraeth <127773881+DaroxTurtle@users.noreply.github.com>
2023-05-06 16:43:26 -0700

    050523.py

---
+++ LeetCode/001_Two_Sum.py
@@ -0,0 +1,7 @@
+print('001_Two_Sum')
+
+# Given an array of integers nums and an integer targert, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
